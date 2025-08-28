1290774e32107593292ba882cb3d8de50458e1c1
sandeep <sandeepshome10@gmail.com>
2025-08-28 21:21:40 +0530

    types: make href optional

--- src/types/products.tsx
+++ src/types/products.tsx
@@ -5,8 +5,8 @@ export type Product = {
   description: string;
   thumbnail: StaticImageData;
   images: StaticImageData[] | string[];
-  href?: string;
-  slug?: string;
+  href: string;
+  slug: string;
   linkPlaceholder?: string;
   stack?: string[];
   content?: React.ReactNode | string;
